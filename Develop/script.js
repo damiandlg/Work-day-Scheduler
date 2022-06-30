20deb3fa74cc42c4b85c362913e91bd8501084e2
damiandlg <damian.delagarzaf@gmail.com>
2022-06-30 01:11:23 -0400

    Event creation popo-up with bootstrap

--- Develop/script.js
+++ Develop/script.js
@@ -3,53 +3,50 @@
  */
 
 class TimeSlot {
+    timeSlotId = ""
     name = ""
-    time = 0
+    hour = 0
     slotState = {
         past: false,
         present: false,
         future: false
     }
 
-    constructor(index) {
-        if (index == 0) {
-            this.name = "9 am"
-            this.time = 9
-        }
-        if (index == 1) {
-            this.name = "10 am"
-            this.time = 10
-        }
-        if (index == 2) {
-            this.name = "11 am"
-            this.time = 11
-        }
-        if (index == 3) {
-            this.name = "12 pm"
-            this.time = 9
-        }
-        if (index == 4) {
-            this.name = "1 pm"
-            this.time = 1
-        }
-        if (index == 5) {
-            this.name = "2 pm"
-            this.time = 2
-        }
-        if (index == 6) {
-            this.name = "3 pm"
-            this.time = 3
-        }
-        if (index == 7) {
-            this.name = "4 pm"
-            this.time = 4
-        }
-        if (index == 8) {
-            this.name = "5 pm"
-            this.time = 5
-        }
+    constructor(hour, time) {
+        //set hour
+        this.hour = hour
+
+        //generate standard military time
+        const timeOfDay = (hour > 12) ? "PM" : "AM"
+        const t = ((hour + 11) % 12 + 1)
+        
+        //set display name
+        this.name = `${t} ${timeOfDay}`
+
+        //set time slot Id
+        this.timeSlotId = `${t}-${timeOfDay}`
+
+        //set slot state
+       this.setSlotState(time)
     }
-}
+
+    setSlotState(time) {
+      this.slotState.present = (this.hour === time)
+      this.slotState.past = (this.hour < time)
+      this.slotState.future = (this.hour > time)
+      } 
+      getColor() {
+        if(this.slotState.present)
+            return "red"
+        if(this.slotState.future)
+            return "green"
+        if(this.slotState.past)
+            return"gray"
+    }
+    }
+
+  
+
 
 
 /**
@@ -58,45 +55,76 @@ class TimeSlot {
 
 class AppState {
     currentDate = new Date()
-    timeSlots = []
+    timeSlots = {}
 
     constructor() {
         this.generateTimeSlots()
     }
     generateTimeSlots() {
-        for ( let i=0; i<=8; i++) {
-            const timeSlot = new TimeSlot(i)
-            this.timeSlots.push(timeSlot)
+        for (let i=9; i<=17; i++) {
+            //For submission remember to pass this.currentDate.getHours()
+            //into our TimeSlot class instead of dummy data
+            const timeSlot = new TimeSlot(i, 15)
+            this.timeSlots[timeSlot.timeSlotId] = timeSlot
         }
     }
 }
 
-
+//Initialize AppState
 const state= new AppState()
-console.log(state.timeSlots)
 
 
-printDate(state.currentDate)
+
+
+/**
+ * Render App State on UI
+ */
 var dateElement = document.getElementById("currentDay")
 dateElement.innerText = state.currentDate.toDateString()
  
-
+//Grab UL elemnt from HTMl dom
 var timeBlock = document.getElementById("timeBlock")
- for (let timeSlot of state.timeSlots){
-    let item = document.createElement ("li")
+
+//Generate an li element for ach timeSlot in out state time slot list
+ for (const [key, timeSlot] of Object.entries(state.timeSlots)){
+    const timeSlotElement = createTimeSlotElement(timeSlot)
+    timeBlock.appendChild(timeSlotElement)
+ }
+
+
+ //Create unique time slot elements
+ function createTimeSlotElement(timeSlot) {
+ let item = document.createElement ("li")
+    item.id = timeSlot.timeSlotId
     item.className = "list-group-item"
     item.innerHTML = timeSlot.name
-    timeBlock.appendChild(item)
+    item.style.background = timeSlot.getColor()
+
+    //Handle Click Events
+    item.addEventListener('click', (e) => {
+        const timeSlotId = e.srcElement.id
+        console.log(timeSlotId)
+        $('#exampleModal').modal('show');
+        const modal = document.getElementById("modalBody")
+       // modal.innerHTML = state.timeSlots[timeSlotId].name
+    })
+
+
+    return item
  }
 
 /**
  * Logs
  */
+ console.log(state.timeSlots)
+ printDate(state.currentDate)
+
 
 function printDate(date) {
     const year = date.getFullYear();
     const day = date.getDay();
     const month = date.getMonth();
+    const hour = date.getHours();
     const dateString = date.toDateString();
     const dateJson = date.toJSON();
 
